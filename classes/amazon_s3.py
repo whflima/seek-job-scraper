1ba9f09ec75aaa369186a2a164474b698789a89e
Welisson Hudson Fernandes Lima <wellissonsacer2000@gmail.com>
2025-05-08 21:28:27 -0300

    feat: set lambda environment variables.

--- classes/amazon_s3.py
+++ classes/amazon_s3.py
@@ -4,15 +4,14 @@ import os
 
 KEY = os.environ.get('AWS_FILE_BUCKET_KEY')
 BUCKET = os.environ.get('AWS_BUCKET_NAME') 
+REGION = os.environ.get('AWS_REGION')
 LOCAL_STORAGE = "web_scraping.db" #os.environ.get('KEY') "/tmp/web_scraping.db"
-ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
-SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 class AmazonS3Storage():
     def __init__(self, key: str = KEY, bucket: str = BUCKET):
         self.key = key
         self.bucket = bucket
-        self.s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+        self.s3 = boto3.client('s3', region_name=REGION)
     
     def download_file(self, local_file: str = LOCAL_STORAGE):
         return self.s3.download_file(self.bucket, self.key, local_file)
