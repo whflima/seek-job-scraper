3fe3cda48c66a8a0de7b8382df2aea9f3d4be004
Welisson Hudson Fernandes Lima <wellissonsacer2000@gmail.com>
2025-05-09 02:35:04 -0300

    feat: update dockerfile.

--- api-web-scraper/app/main.py
+++ api-web-scraper/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import uvicorn
 
 
 app = FastAPI()
@@ -9,3 +10,6 @@ async def root():
     return {"message": "Hello World"}
 
 handler = Mangum(app)
+
+if __name__ == "__main__":
+   uvicorn.run(app, host="0.0.0.0", port=8080)
